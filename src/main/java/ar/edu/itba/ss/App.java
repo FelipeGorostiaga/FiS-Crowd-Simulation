e3750b5ced9fccd6b1b2427d633e70853b739fe5
Felipe Gorostiaga <fgorostiaga@itba.edu.ar>
2020-04-28 19:03:38 -0300

    Print to file

--- src/main/java/ar/edu/itba/ss/App.java
+++ src/main/java/ar/edu/itba/ss/App.java
@@ -1,5 +1,10 @@
 package ar.edu.itba.ss;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+
 import static ar.edu.itba.ss.CommandLineParser.FPS;
 import static ar.edu.itba.ss.CommandLineParser.dt;
 import static ar.edu.itba.ss.Generator.particles;
@@ -7,6 +12,13 @@ import static ar.edu.itba.ss.Generator.particles;
 public class App {
 
     public static void main( String[] args ) {
+
+        try {
+            File file = new File("output.xyz");
+            System.setOut(new PrintStream(new FileOutputStream(file)));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
 
         CommandLineParser.parseOptions(args);
         Generator.generateParticles();
